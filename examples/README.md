32474c49c298b7d544700a0bd0b40a96bc0850d0
Sebastian Daberdaku <sebastian.daberdaku@cardoai.com>
2024-01-26 18:04:09 +0100

    improved examples

--- examples/README.md
+++ examples/README.md
@@ -6,7 +6,7 @@ the available example Apache Airflow DAGs.
 * kubectl (setup instructions [here](https://kubernetes.io/docs/tasks/tools/));
 * minikube (setup instructions [here](https://minikube.sigs.k8s.io/docs/start/));
 * Helm (setup instructions [here](https://github.com/helm/helm#install));
-* (Optional) [Kubernetes Dashboard](https://minikube.sigs.k8s.io/docs/handbook/dashboard/), [Open Lens](https://flathub.org/it/apps/dev.k8slens.OpenLens) or [k9s](https://k9scli.io/) or any Kubernetes management tool.
+* (Optional) [Kubernetes Dashboard](https://minikube.sigs.k8s.io/docs/handbook/dashboard/), [Open Lens](https://flathub.org/it/apps/dev.k8slens.OpenLens), [k9s](https://k9scli.io/) or any Kubernetes management tool.
 
 ## Starting the minikube cluster
 
@@ -35,9 +35,8 @@ minikube mount ./dags/:/mnt/airflow/dags
 Install Airflow using an image with our provider pre-installed.
 ```shell
 helm repo add apache-airflow https://airflow.apache.org
-helm install airflow apache-airflow/airflow \
+helm upgrade --install airflow apache-airflow/airflow \
   --version 1.11.0 \
-  --wait \
   --timeout 15m \
   --namespace airflow \
   --create-namespace \
@@ -48,13 +47,18 @@ helm install airflow apache-airflow/airflow \
   --set images.airflow.pullPolicy=Always \
   --set executor=KubernetesExecutor \
   --set dags.persistence.enabled=true \
-  --set dags.persistence.existingClaim=dags
+  --set dags.persistence.existingClaim=dags \
+  --set logs.persistence.enabled=true \
+  --set logs.persistence.size=5Gi \
+  --set webserverSecretKey=some_fancy_long_secret_key_string \
+  --set "env[0].name=AIRFLOW_CONN_AWS" \
+  --set "env[0].value='aws://@/?aws_access_key_id=foo&aws_secret_access_key=bar&region_name=eu-central-1&endpoint_url=http%3A%2F%2Flocalstack.localstack.svc.cluster.local%3A4566&verify=False'"
 ```
 
 Install the `pysparkonk8s-addon` Helm chart.
 ```shell
 helm repo add pysparkonk8s https://sebastiandaberdaku.github.io/apache-airflow-providers-pysparkonk8s
-helm install pysparkonk8s pysparkonk8s/pysparkonk8s-addon \
+helm upgrade --install pysparkonk8s pysparkonk8s/pysparkonk8s-addon \
   --version 1.0.0 \
   --namespace airflow \
   --set workerServiceAccount=airflow-worker
@@ -64,9 +68,8 @@ Install [LocalStack](https://docs.localstack.cloud/overview/) to simulate the AW
 
 ```shell
 helm repo add localstack https://localstack.github.io/helm-charts
-helm install localstack localstack/localstack \
+helm upgrade --install localstack localstack/localstack \
   --version 0.6.8 \
-  --wait \
   --timeout 15m \
   --namespace localstack \
   --create-namespace 
--- examples/dags/example_dags.py
+++ examples/dags/example_dags.py
@@ -1,36 +1,100 @@
-from time import sleep
+import logging
 from typing import TYPE_CHECKING
 
+import requests
 from airflow.decorators import dag, task
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.pysparkonk8s.config import SparkBaseConf
 from pendulum import datetime
 
 if TYPE_CHECKING:
     from pyspark.sql import SparkSession
 
 
-@task.pyspark_on_k8s
-def extract(spark: "SparkSession") -> float:
-    from pyspark import SparkFiles
-    from pyspark.sql import functions as f
+logger = logging.getLogger(__name__)
 
-    url = "https://people.sc.fsu.edu/~jburkardt/data/csv/deniro.csv"
-    spark.sparkContext.addFile(url)
-    df = spark.read.csv("file://" + SparkFiles.get("deniro.csv"), header=True, inferSchema=True)
-    df.show()
+@task()
+def create_bucket(s3_bucket: str, region: str) -> None:
+    """
+    Creates an S3 bucket if it does not already exist.
 
-    sleep(120)
+    :param s3_bucket: The name of the S3 bucket to be created.
+    :param region: The AWS region in which the S3 bucket should be created.
+    :return: None
+    """
+    s3_hook = S3Hook(aws_conn_id="aws")
+    logger.info(f"Checking if S3 bucket {s3_bucket} exists...")
+    if not s3_hook.check_for_bucket(bucket_name=s3_bucket):
+        logger.info(f"S3 bucket {s3_bucket} does not exist. Creating...")
+        s3_hook.create_bucket(bucket_name=s3_bucket, region_name=region)
+        logger.info(f"Created S3 bucket {s3_bucket} in region {region}.")
+    else:
+        logger.info(f"S3 bucket {s3_bucket} already exists.")
 
-    mean_score, = df.select(f.mean("Score")).first()
-    return mean_score
+@task()
+def extract(url: str, s3_bucket: str) -> str:
+    """
+    Downloads a file from the specified URL and uploads it to an S3 bucket.
+
+    :param url: The URL of the file to be downloaded.
+    :param s3_bucket: The name of the S3 bucket where the file will be uploaded.
+    :return: The S3 key of the uploaded file.
+    """
+    s3_hook = S3Hook(aws_conn_id="aws")
+    filename = url.split("/")[-1]
+    s3_key = f"extracted/{filename}"
+    with requests.get(url, stream=True) as response:
+        response.raise_for_status()
+        response.raw.decode_content = True
+        s3_hook.load_file_obj(file_obj=response.raw, key=s3_key, bucket_name=s3_bucket, replace=True)
+    return s3_key
+
+
+S3_ENDPOINT = "http://localstack.localstack.svc.cluster.local:4566"
+sbc = SparkBaseConf(jars=["com.amazonaws:aws-java-sdk-bundle:1.12.262", "org.apache.hadoop:hadoop-aws:3.3.4"])
+extra_conf = {
+    "spark.hadoop.fs.s3a.impl": "org.apache.hadoop.fs.s3a.S3AFileSystem",
+    "spark.hadoop.fs.s3a.endpoint": S3_ENDPOINT,
+    "spark.hadoop.fs.s3a.path.style.access": "true",
+    "spark.hadoop.fs.s3a.connection.ssl.enabled": "false",
+    "spark.hadoop.fs.s3a.access.key": "foo",
+    "spark.hadoop.fs.s3a.secret.key": "bar",
+}
+
+@task.pyspark_on_k8s(spark_base_conf=sbc, spark_extra_conf=extra_conf)
+def load(s3_bucket: str, s3_key: str, spark: "SparkSession") -> str:
+    """
+    Loads the given CSV file on S3 to parquet format.
+    :param s3_bucket: The name of the S3 bucket where the file will be uploaded.
+    :param s3_key: The S3 key of the CSV file to load.
+    :param spark: SparkSession object provided by the @task.pyspark_on_k8s decorator
+    :return: S3A destination URI of the resulting parquet files.
+    """
+    s3a_uri = f"s3a://{s3_bucket}/{s3_key}"
+    df = spark.read.csv(s3a_uri, header=True, inferSchema=True)
+    print(df.head(5))
+
+    s3a_dst_uri = f"s3a://{s3_bucket}/parquet/"
+    df.write.mode("overwrite").parquet(s3a_dst_uri)
+    return s3a_dst_uri
 
 
 @dag(
     dag_id="example_dag_1",
     start_date=datetime(2024, 1, 1),
     schedule=None,
+    params={
+        "url": "https://people.sc.fsu.edu/~jburkardt/data/csv/deniro.csv",
+        "s3_bucket": "test-bucket",
+        "region": "eu-central-1",
+    }
 )
 def example_dag_1():
-    mean_score = extract()
+    csv_s3_key = extract(url="{{ params.url }}", s3_bucket="{{ params.s3_bucket }}")
+
+    create_bucket(s3_bucket="{{ params.s3_bucket }}", region="{{ params.region }}") >> csv_s3_key
+
+    load(s3_bucket="{{ params.s3_bucket }}", s3_key=csv_s3_key)
 
 
 example_dag_1()
